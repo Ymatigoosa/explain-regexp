6f55055c483ba5ed0c51099ebe34c7ad3ac1a1b2
7 <7@7-PC>
2012-06-14 18:08:17 +0400

    ???????? ???????, ???????????

--- nodepattern.h
+++ nodepattern.h
@@ -1,10 +1,16 @@
 #ifndef NODEPATTERN_H
 #define NODEPATTERN_H
 
+/*!
+ * Класс, список шаблонов для определенного узла
+ */
 class nodePattern
 {
 public:
     nodePattern();
+
+	QString * default; //!< стандартный шаблон;
+	QList<variant *> variants; //!< список альтернативных шаблонов.
 };
 
 #endif // NODEPATTERN_H
