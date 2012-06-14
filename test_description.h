518d0666a3066ae79debcc4687c2b257751bd295
7 <7@7-PC>
2012-06-14 17:22:10 +0400

    ????????? ????? ??? ??????

---
+++ test_description.h
@@ -0,0 +1,14 @@
+#ifndef TEST_DESCRIPTION_H
+#define TEST_DESCRIPTION_H
+
+#include <QObject>
+#include <QtTest/QtTest>
+
+class test_description : public QObject
+{
+    Q_OBJECT
+private slots:
+    
+};
+
+#endif // TEST_DESCRIPTION_H
