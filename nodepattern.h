d8b3b5154cd5ef3229f0c0ce57910d7cf9b06758
7 <7@7-PC>
2012-06-16 22:42:01 +0400

    ?????????? ???????? ??????????(default - ????????????????? ?????)

--- nodepattern.h
+++ nodepattern.h
@@ -3,16 +3,16 @@
 #include "variant.h"
 
 /*!
- * \brief список шаблонов для определенного узла
- * Класс, список шаблонов для определенного узла
+ * \brief список шаблонов для определенного узла.
+ * Класс, список шаблонов для определенного узла. defaultPattern - стандартный шаблон. variantPatterns - массив альтернативных шаблонов
  */
 class nodePattern
 {
 public:
     nodePattern();
 
-	QString * default; //!< стандартный шаблон;
-	QList<variant *> variants; //!< список альтернативных шаблонов.
+	QString * defaultPattern; //!< стандартный шаблон;
+	QList<variant *> variantPatterns; //!< список альтернативных шаблонов.
 };
 
 #endif // NODEPATTERN_H
